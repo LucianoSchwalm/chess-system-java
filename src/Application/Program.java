4acaad953c5169e786ee2e107b6407680c77cad1
LucianoSchwalm <luciano.rocha21@hotmail.com>
2022-02-18 16:53:17 -0300

    Stating to implement Board and Piece 2

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,11 +2,13 @@ package Application;
 
 import BoardGame.Board;
 import BoardGame.Position;
+import BoardGame.Piece;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Board board = new Board(8, 8);
+		
+		Board board = new Board(8,8);
 
 	}
 
